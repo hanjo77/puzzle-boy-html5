2a2c8ae5abbac203f5a33ebf542614be1103ffad
Hansjürg Jaggi <hanjo77@gmail.com>
2015-01-20 06:31:14 -0800

    Added some data

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Puzzle Boy</title>
+		<link href="puzzle-boy.css" type="text/css" rel="stylesheet"></link>
+		<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+		<script src="puzzle-boy.js"></script>
+	</head>
+	<body>
+		<canvas id="playground"></canvas>
+	</body>
+</html>
