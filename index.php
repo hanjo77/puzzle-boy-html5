6008d21c86b8b9540bb58b8e65c4522e4782de08
Hanjo <hanjo77@gmail.com>
2015-01-23 20:31:25 +0100

    More mobile updates and finetuning

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta name="viewport" content="width=480, initial-scale=1.5">
+		<meta name="viewport" content="width=device-width, initial-scale=0.6">
 		<title>Puzzle Boy</title>
 		<link href="css/puzzle-boy.css" type="text/css" rel="stylesheet"></link>
 		<script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
 		<script src="js/puzzle-boy.js"></script>
 	</head>
 	<body>
-		<a href="#level0" onclick="loadLevel(levels[0])"><img src="img/title.png" /></a>
+		<a href="#level0" onclick="loadLevel(levels[0])" id="title"><img src="img/title.png" /></a>
 	</body>
 </html>
